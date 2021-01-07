037590973b01a31be1dbb86a9a4fba0fc69a9bec
renata2014 <x.rena_x@yahoo.com>
2021-01-07 17:48:22 +0200

    pics url done

--- src/pages/hobbies/Hobbies.js
+++ src/pages/hobbies/Hobbies.js
@@ -14,25 +14,25 @@ export class AboutMePage extends React.Component{
   </div>
                <h3 className="title-texth3">My <span className="title-spanh3">Drawings</span></h3>
                <Carousel 
-                    PhotoSrc1="https://scontent.fclj3-1.fna.fbcdn.net/v/t1.15752-0/p280x280/123364142_1554543151414291_8079478411326522501_n.jpg?_nc_cat=107&ccb=2&_nc_sid=ae9488&_nc_ohc=chixXRVCaAEAX-9D39b&_nc_ht=scontent.fclj3-1.fna&tp=6&oh=994c201f532da617b4a93a4d088db68e&oe=5FF41E3B"
-                    PhotoSrc2="https://scontent.fclj3-1.fna.fbcdn.net/v/t1.15752-0/p280x280/123191336_784443885731490_7899108489426619321_n.jpg?_nc_cat=108&ccb=2&_nc_sid=ae9488&_nc_ohc=wiw4ml1OBn0AX87WUSR&_nc_ht=scontent.fclj3-1.fna&tp=6&oh=1c090da66b524e21ad03ddda6c48fdb6&oe=5FF51879"
-                    PhotoSrc3="https://scontent.fclj3-1.fna.fbcdn.net/v/l/t1.15752-0/p280x280/123141571_854110528461313_7814110398487689958_n.jpg?_nc_cat=107&ccb=2&_nc_sid=ae9488&_nc_ohc=HoyD4Lc5TacAX_vnEjc&_nc_ht=scontent.fclj3-1.fna&tp=6&oh=1f4d410ca03973fe93c864d0d8750817&oe=5FF20648"
-                    PhotoSrc4="https://scontent.fclj3-1.fna.fbcdn.net/v/t1.15752-0/p280x280/123140430_364274658358661_2442177403878400047_n.jpg?_nc_cat=109&ccb=2&_nc_sid=ae9488&_nc_ohc=jdaqssrcy9wAX-ichVU&_nc_ht=scontent.fclj3-1.fna&tp=6&oh=a616a1c641342abc948f6728c944ad9a&oe=5FF4E595"
-                    PhotoSrc5="https://scontent.fclj3-1.fna.fbcdn.net/v/t1.15752-0/p280x280/123315433_1245444455829781_8697685483638967546_n.jpg?_nc_cat=107&ccb=2&_nc_sid=ae9488&_nc_ohc=rUp1Tp4JJ7sAX9pjXw1&_nc_ht=scontent.fclj3-1.fna&tp=6&oh=fb82712059267afc19888bbe5a458c41&oe=5FF272D6"
-                    PhotoSrc6="https://scontent.fclj3-1.fna.fbcdn.net/v/t1.15752-0/p280x280/123210800_1351867271824470_7561442316029057461_n.jpg?_nc_cat=100&ccb=2&_nc_sid=ae9488&_nc_ohc=7oLaIBriXmcAX-Hdg6A&_nc_ht=scontent.fclj3-1.fna&tp=6&oh=5a65beaf07eea5d09c64148cdc7787fb&oe=5FF3E10E"
-                    PhotoSrc7="https://scontent.fclj3-1.fna.fbcdn.net/v/t1.15752-0/p280x280/123136470_365313128076691_688394645377449446_n.jpg?_nc_cat=104&ccb=2&_nc_sid=ae9488&_nc_ohc=unKhj_WrxfQAX90Qq6m&_nc_ht=scontent.fclj3-1.fna&tp=6&oh=f0a999bdfbbcbb3244b95331bf3308ce&oe=5FF561D1"
-                    PhotoSrc8="https://scontent.fclj3-1.fna.fbcdn.net/v/t1.15752-0/p280x280/123135470_712982029311468_8395029186303974247_n.jpg?_nc_cat=111&ccb=2&_nc_sid=ae9488&_nc_ohc=v3hU5OrAI20AX_b9JmB&_nc_ht=scontent.fclj3-1.fna&tp=6&oh=89a3acf442f592b916ec1025286c85cc&oe=5FF25637"
+                    PhotoSrc1="https://www.linkpicture.com/q/123315433_1245444455829781_8697685483638967546_n.jpg"
+                    PhotoSrc2="https://www.linkpicture.com/q/123364142_1554543151414291_8079478411326522501_n.jpg"
+                    PhotoSrc3="https://www.linkpicture.com/q/123191336_784443885731490_7899108489426619321_n.jpg"
+                    PhotoSrc4="https://www.linkpicture.com/q/123140430_364274658358661_2442177403878400047_n.jpg"
+                    PhotoSrc5="https://www.linkpicture.com/q/123135470_712982029311468_8395029186303974247_n.jpg"
+                    PhotoSrc6="https://www.linkpicture.com/q/123141571_854110528461313_7814110398487689958_n.jpg"
+                    PhotoSrc7="https://www.linkpicture.com/q/123136470_365313128076691_688394645377449446_n.jpg"
+                    PhotoSrc8="https://www.linkpicture.com/q/123210800_1351867271824470_7561442316029057461_n.jpg"
                     />
                <h3 className="title-texth3">Some <span className="title-spanh3">Travel Photos</span></h3>
                <Carousel 
-                  PhotoSrc1="https://scontent.fclj3-1.fna.fbcdn.net/v/t1.15752-9/123143919_3786677774699984_7754362684216471150_n.png?_nc_cat=110ccb=2&_nc_sid=ae9488&_nc_ohc=J2ze9gcyoLkAX86v0h5&_nc_ht=scontent.fclj3-1.fna&oh=0bb93be6a59ae2623f18781f847b2a42&oe=5FF33923"
-                  PhotoSrc2="https://scontent.fclj3-1.fna.fbcdn.net/v/t1.15752-0/p280x280/123207561_690382001879939_3019926726907842237_n.jpg?_nc_cat=108&ccb=2&_nc_sid=ae9488&_nc_ohc=G8tN1Xh_UWgAX-k37Ko&_nc_ht=scontent.fclj3-1.fna&tp=6&oh=f8c55187653a3782224e6253b7979854&oe=5FF32578"
-                  PhotoSrc3="https://scontent.fclj3-1.fna.fbcdn.net/v/t1.15752-0/p280x280/123258730_1067783193659146_7178326516428683522_n.png?_nc_cat=106&ccb=2&_nc_sid=ae9488&_nc_ohc=D1tewsuypvIAX8nuMuh&_nc_ht=scontent.fclj3-1.fna&_nc_tp=30&oh=fa2aeba8212aeeaf673ec6d6064c274f&oe=5FF235B4"
-                  PhotoSrc4="https://scontent.fclj3-1.fna.fbcdn.net/v/t1.15752-0/p280x280/123205413_1056415011484747_8002966518839211854_n.jpg?_nc_cat=106&ccb=2&_nc_sid=ae9488&_nc_ohc=t2ZwDJwkOGoAX846XEX&_nc_ht=scontent.fclj3-1.fna&tp=6&oh=dd82ef086c50a5e9b3231e5731b65286&oe=5FF31101"
-                  PhotoSrc5="https://scontent.fclj3-1.fna.fbcdn.net/v/t1.15752-0/p280x280/123146547_1603627443155822_7188058427487343478_n.jpg?_nc_cat=111&ccb=2&_nc_sid=ae9488&_nc_ohc=zTxmYNAPdpgAX_nIhcx&_nc_ht=scontent.fclj3-1.fna&tp=6&oh=4fe95cbfe23bd352bc4de66035f56ba3&oe=5FF3027C"
-                  PhotoSrc6="https://scontent.fclj3-1.fna.fbcdn.net/v/t1.15752-0/p280x280/123187021_838585926943792_6159406328428007697_n.jpg?_nc_cat=101&ccb=2&_nc_sid=ae9488&_nc_ohc=yOhpRvn357MAX_FkmIx&_nc_ht=scontent.fclj3-1.fna&tp=6&oh=39801251f4876d652460ac5bcc9231c6&oe=5FF4B951"
-                  PhotoSrc7="https://scontent.fclj3-1.fna.fbcdn.net/v/t1.15752-0/p280x280/123299261_365263621580096_1972239855327278925_n.jpg?_nc_cat=106&ccb=2&_nc_sid=ae9488&_nc_ohc=fD_VofvmenEAX98yOU_&_nc_ht=scontent.fclj3-1.fna&tp=6&oh=7e8a96ba9b72eb08986a2d34dff23e08&oe=5FF37C0B"
-                  PhotoSrc8="https://scontent.fclj3-1.fna.fbcdn.net/v/t1.15752-0/p280x280/123145873_812448949297913_8435747944162122243_n.jpg?_nc_cat=108&ccb=2&_nc_sid=ae9488&_nc_ohc=zD1Rpg91Iz0AX8MJ0JA&_nc_ht=scontent.fclj3-1.fna&tp=6&oh=3c68f575e4ad43b6238d12a8a0576aeb&oe=5FF3FDF8"
+                  PhotoSrc1="https://www.linkpicture.com/q/123142861_1040688756375674_8073785683435306275_n.png"
+                  PhotoSrc2="https://www.linkpicture.com/q/123258730_1067783193659146_7178326516428683522_n.png"
+                  PhotoSrc3="https://www.linkpicture.com/q/123299261_365263621580096_1972239855327278925_n.jpg"
+                  PhotoSrc4="https://www.linkpicture.com/q/123243944_856713708403620_8633167813246414454_n.jpg"
+                  PhotoSrc5="https://www.linkpicture.com/q/123207561_690382001879939_3019926726907842237_n.jpg"
+                  PhotoSrc6="https://www.linkpicture.com/q/123145873_812448949297913_8435747944162122243_n.jpg"
+                  PhotoSrc7="https://www.linkpicture.com/q/123146547_1603627443155822_7188058427487343478_n.jpg"
+                  PhotoSrc8="https://www.linkpicture.com/q/123187021_838585926943792_6159406328428007697_n.jpg"
                   />
           </div>
         )
